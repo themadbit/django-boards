1ba635d7167f61a1b2c6bf624053cad6c744ae85
themadbit <marktanui75@gmail.com>
2025-07-16 23:16:48 +0300

    init topics

--- djangoNix/djangoNix/urls.py
+++ djangoNix/djangoNix/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from boards import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
