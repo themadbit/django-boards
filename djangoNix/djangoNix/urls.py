35d16485b2f9dfcb8b8bc5d11b4b27651391bc08
themadbit <marktanui75@gmail.com>
2025-08-18 17:56:26 +0300

    add signup path

--- djangoNix/djangoNix/urls.py
+++ djangoNix/djangoNix/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path, re_path
 
 from boards import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+
+    # Accounts urls
+    re_path(r'^signup/$', accounts_views.signup, name='signup'),
 ]
