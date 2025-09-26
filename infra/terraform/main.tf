0657a144d3b438ff4c8ef6a34e451543230b3923
themadbit <marktanui75@gmail.com>
2025-09-26 12:33:44 +0300

    infra: allow grafana port 3000

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -70,6 +70,19 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "GRAFANA"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 
 resource "azurerm_public_ip" "main" {
